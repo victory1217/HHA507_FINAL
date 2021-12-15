1104b6712a57fcd3d3390cc706daaf1c024e9a1b
Victoria Rodriguez <victoria.inbox77@gmail.com>
2021-12-14 21:20:50 -0500

    Including streamlit contents

--- HHA507_FINAL.py
+++ HHA507_FINAL.py
@@ -6,6 +6,8 @@ Created on Sun Dec 12 19:20:05 2021
 @author: victoria_rodriguez
 """
 
+## PART 1 - DATA EXTRACTION PORTION
+
 ## Step 1 - Import needed packages 
 
 import pandas as pd
@@ -219,3 +221,146 @@ NY_outpatient = outpatientdf[outpatientdf['provider_state'] == 'NY']
 ###4. Which DRG service has the highest total discharges for Stony Brook?
 ###5. Which APC service has the highest total discharges for Stony Brook?
 ###6. What are Stony Brook's quality comparison levels in comparison to Albany Medical Center Hospital? 
+-------------------------------------------------------------------------------------------------------------------
+### PART 2 - STREAMLIT PORTION
+
+
+## Import needed packages 
+
+!pip install streamlit
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff 
+import time
+import scipy 
+
+
+## Load in the 3 primary csv files for display  
+
+@st.cache
+def load_hospitals():
+   hospitaldf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
+    return hospitaldf
+
+@st.cache
+def load_inatpatient():
+    inpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
+    return inpatientdf
+
+@st.cache
+def load_outpatient():
+    outpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
+    return outpatientdf
+    
+    
+# FAKE LOADER BAR TO STIMULATE LOADING    
+# my_bar = st.progress(0)
+# for percent_complete in range(100):
+#     time.sleep(0.1)
+#     my_bar.progress(percent_complete + 1)
+  
+
+st.write('Hello, *World!* :sunglasses:') 
+  
+# Load the data:     
+hospitaldf = load_hospitals()
+inpatientdf = load_inatpatient()
+outpatientdf = load_outpatient()
+
+
+# Preview the dataframes 
+st.header('Hospital Data Preview')
+st.dataframe(hospitaldf)
+
+st.header('Inpatient Data Preview')
+st.dataframe(inpatientdf)
+
+st.header('Outpatient Data Preview')
+st.dataframe(outpatientdf)
+
+
+# Create a pivot table 
+st.subheader('Hospital Data Pivot Table')
+dataframe_pivot = hospitaldf.pivot_table(index=['state','city'],values=['hospital_overall_rating'],aggfunc='mean')
+st.dataframe(dataframe_pivot)
+st.markdown('Stony Brook University Hospital is ranked 4, so this provides perspective on how they compare to other hospitals.')
+
+# Create a unique dataframe for New York Hospitals 
+hospitals_ny = hospitaldf[hospitaldf['state'] == 'NY']
+
+# Create a bar chart showing the common hospital types 
+st.subheader('Hospital Type - NY')
+bar1 = hospitals_ny['hospital_type'].value_counts().reset_index()
+st.dataframe(bar1)
+
+st.markdown('The majority of hospitals in NY are acute care, which includes our point of comparison, Stony Brook University Hospital.')
+
+
+st.subheader('Map of NY Hospital Locations')
+
+hospitals_ny_gps = hospitals_ny['location'].str.strip('()').str.split(' ', expand=True).rename(columns={0: 'Point', 1:'lon', 2:'lat'}) 	
+hospitals_ny_gps['lon'] = hospitals_ny_gps['lon'].str.strip('(')
+hospitals_ny_gps = hospitals_ny_gps.dropna()
+hospitals_ny_gps['lon'] = pd.to_numeric(hospitals_ny_gps['lon'])
+hospitals_ny_gps['lat'] = pd.to_numeric(hospitals_ny_gps['lat'])
+
+st.map(hospitals_ny_gps)
+
+## Identifying the common most common discharge levels  
+
+inpatient_ny = inpatientdf[inpatientdf['provider_state'] == 'NY']
+outpatient_ny = outpatientdf[outpatientdf['provider_state'] == 'NY']
+
+
+inpatient_discharges = inpatient_ny.groupby('drg_definition')['total_discharges'].sum().reset_index()
+outpatient_discharges = outpatient_ny.groupby('apc_definition')['total_discharges'].sum().reset_index()
+
+
+top10 = inpatient_discharges.head(10)
+bottom10 = inpatient_discharges.tail(10)
+
+
+top_10 = outpatient_discharges.head(10)
+bottom_10 = outpatient_discharges.tail(10)
+
+st.header('Discharge Summary')
+st.dataframe(inpatient_discharges)
+st.dataframe(outpatient_discharges)
+
+
+col1, col2 = st.beta_columns(2)
+
+col1.header('Top 10 DRGs')
+col1.dataframe(top10)
+
+col2.header('Bottom 10 DRGs')
+col2.dataframe(bottom10)
+
+col1.header('Top 10 DRGs')
+col1.dataframe(top_10)
+
+col2.header('Bottom 10 DRGs')
+col2.dataframe(bottom_10)
+
+st.markdown('This answers the question of which DRG and APC services have the highest discharges.')
+
+# Created bar charts for the average payment costs for inpatient 
+
+costs = inpatient_ny.groupby('provider_name')['avsterage_total_payments'].sum().reset_index()
+costs['average_total_payments'] = costs['average_total_payments'].astype('int64')
+
+
+costs_medicare = inpatient_ny.groupby('provider_name')['average_medicare_payments'].sum().reset_index()
+costs_medicare['average_medicare_payments'] = costs_medicare['average_medicare_payments'].astype('int64')
+
+
+costs_sum = costs.merge(costs_medicare, how='left', left_on='provider_name', right_on='provider_name')
+costs_sum['delta'] = costs_sum['average_total_payments'] - costs_sum['average_medicare_payments']
+
+
+st.title('Costs for Inpatient Services')
+
+bar3 = px.bar(costs_sum, x='provider_name', y='average_total_payments')
+st.plotly_chart(bar3)
+st.header("Hospital - ")
+st.dataframe(costs_sum)
