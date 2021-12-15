5bb5f2fdbccff1c44b56ca91045194eb98ad0ba7
Victoria Rodriguez <victoria.inbox77@gmail.com>
2021-12-14 21:26:58 -0500

    removed return function

--- FINAL_streamlit.py
+++ FINAL_streamlit.py
@@ -21,15 +21,15 @@ import time
 @st.cache
 def load_hospitals():
    hospitaldf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
-return hospitaldf
+
 @st.cache
 def load_inatpatient():
     inpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
-return inpatientdf
+
 @st.cache
 def load_outpatient():
     outpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
-return outpatientdf
+
     
 # FAKE LOADER BAR TO STIMULATE LOADING    
 # my_bar = st.progress(0)
