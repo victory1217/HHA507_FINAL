b931267b5a7e646d7b23b4f48a5f189db90397e5
Victoria Rodriguez <victoria.inbox77@gmail.com>
2021-12-15 18:59:29 -0500

    fix2

--- FINAL_streamlit.py
+++ FINAL_streamlit.py
@@ -83,14 +83,13 @@ st.markdown('Per the table above, you can see that the apc code with the most ou
 
 # Create a unique dataframe for Stony Brook Inpatient info
 sb_inpatient = inpatientdf[inpatientdf['provider_id'] == '330393']
-sb_discharges = sb_inpatient.groupby('drg_definition')['total_discharges'].sum().rest_index()
+sb_discharges = sb_inpatient.groupby('drg_definition')['total_discharges'].sum()
 st.header('Inpatient Discharges for Stony Brook')
 st.dataframe(sb_discharges)
 
 # Create a unique dataframe for Stony Brook Outpatient info
 sb_outpatient = outpatientdf[outpatientdf['provider_id'] == '330393']
-sb_services = sb_outpatient.groupby('apc')['outpatient_services'].sum().rest_index()
-st.header('Outpatient Services for Stony Brook')
+sb_services = sb_outpatient.groupby('apc')['outpatient_services'].sum()
 st.dataframe(sb_services)
 
 
