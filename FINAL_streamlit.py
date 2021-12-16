e5642af8c628d85de36173acd921e2453c519844
Victoria Rodriguez <victoria.inbox77@gmail.com>
2021-12-15 19:06:19 -0500

    test pivot table

--- FINAL_streamlit.py
+++ FINAL_streamlit.py
@@ -83,13 +83,13 @@ st.markdown('Per the table above, you can see that the apc code with the most ou
 
 # Create a unique dataframe for Stony Brook Inpatient info
 sb_inpatient = inpatientdf[inpatientdf['provider_id'] == '330393']
-sb_discharges = sb_inpatient.groupby('drg_definition')['total_discharges'].sum().reset_index()
+sb_discharges = sb_inpatient.pivot_table(index = ['provider_name', 'drg_definition'], values = ['total_discharges'], aggfunc='mean')
 st.header('Inpatient Discharges for Stony Brook')
 st.dataframe(sb_discharges)
 
 # Create a unique dataframe for Stony Brook Outpatient info
 sb_outpatient = outpatientdf[outpatientdf['provider_id'] == '330393']
-sb_services = sb_outpatient.groupby('apc')['outpatient_services'].sum().reset_index()
+sb_services = sb_outpatient.pivot_table(index = ['provider_name', 'apc'], values = ['outpatient_services'], aggfunc='mean')
 st.header('Outpatient Services for Stony Brook')
 st.dataframe(sb_services)
 
